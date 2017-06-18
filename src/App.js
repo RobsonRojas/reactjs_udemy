5efdf8c8adeb5d4da65ae43bbb858a333a8e8d98
RobsonRojas <robson.rojas@gmail.com>
2017-06-17 22:19:39 -0400

    React components

--- src/App.js
+++ src/App.js
@@ -3,17 +3,90 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      headerText: "Welcome to React",
+      contentText: "To get started, edit <code>src/App.js</code> and save to reload."
+    };
+  }
   render() {
     return (
       <div className="App">
+        {/*<h1>{this.state.headerText}</h1>
+        <p>{this.state.contentText}</p>*/}
+        <Header />
+        < Content />
+      </div>
+    );
+  }
+}
+
+class Header extends Component{
+  render(){
+    return(
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
+    );
+  }
+}
+
+class Content extends Component{
+  constructor(props){
+    super(props);
+
+    this.state = {
+      data: [
+        {
+          "id": 1,
+          "name": "Foo",
+          "age": 35
+        },
+        {
+          "id": 2,
+          "name": "Dri",
+          "age": 25
+        },
+        {
+          "id": 3,
+          "name": "Rob",
+          "age": 32
+        }
+      ]
+    };
+  }
+  render(){
+    return(
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <h1>What is component</h1>
+          <p>In this lecture, we will go over the Component</p>
+          <table>
+            <thead>
+              <th>
+                <td>Id</td><td>Name</td><td>Age</td>
+              </th>
+            </thead>
+            <tbody>
+              {this.state.data.map((person, i) => <TableRow key={i} data={person} />)}
+            </tbody>
+          </table>
         </p>
-      </div>
+    );
+  }
+}
+
+
+class TableRow extends Component{
+  render(){
+    return(
+        <tr>
+          <td>{this.props.data.id}</td>
+          <td>{this.props.data.name}</td>
+          <td>{this.props.data.age}</td>
+        </tr>
     );
   }
 }
