c1da7e45ae1010a4f95c38563580d938aeedc06c
RobsonRojas <robson.rojas@gmail.com>
2017-06-18 22:25:02 -0400

    Lecture: Props validation

--- src/App.js
+++ src/App.js
@@ -16,12 +16,18 @@ class App extends Component {
       <div className="App">
         {/*<h1>{this.state.headerText}</h1>
         <p>{this.state.contentText}</p>*/}
-        <Header />
-        < Content />
-        <Clock />
+        <Header header={this.props.header} />
+        < Content content={this.props.content}/>
+        {/*<Clock increment={1} />*/}
+
       </div>
     );
   }
+}
+
+App.defaultProps = {
+  header: "Default props from the header",
+  content: "Default props from the content"
 }
 
 class Header extends Component{
@@ -29,53 +35,54 @@ class Header extends Component{
     return(
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          {/*<h2>Welcome to React</h2>*/}
+          {this.props.header}
         </div>
     );
   }
 }
 
 class Content extends Component{
-  constructor(props){
-    super(props);
-
-    this.state = {
-      data: [
-        {
-          "id": 1,
-          "name": "Foo",
-          "age": 35
-        },
-        {
-          "id": 2,
-          "name": "Dri",
-          "age": 25
-        },
-        {
-          "id": 3,
-          "name": "Rob",
-          "age": 32
-        }
-      ]
-    };
-  }
   render(){
     return(
-        <p className="App-intro">
+      <div>
+        <div className="App-intro">
           <h1>What is component</h1>
           <p>In this lecture, we will go over the Component</p>
-          <table>
-            <thead>
-              <th>
-                <td>Id</td><td>Name</td><td>Age</td>
-              </th>
-            </thead>
-            <tbody>
-              {this.state.data.map((person, i) => <TableRow key={i} data={person} />)}
-            </tbody>
-          </table>
-        </p>
+        </div>
+        <div>
+          <h4>Array: {this.props.propArray}</h4>
+          <h4>Bool: {this.props.propBool ? "True": "False"}</h4>
+          <h4>Function: {this.props.propFunc(5)}</h4>
+          <h4>Number: {this.props.propNumber}</h4>
+          <h4>String: {this.props.propString}</h4>
+          <h4>Object: {this.props.propObject.objectName1}</h4>
+
+        </div>
+      </div>
     );
+  }
+}
+
+Content.propTypes ={
+  propArray: React.PropTypes.array.isRequired,
+  propBool: React.PropTypes.bool.isRequired,
+  propFunc: React.PropTypes.func,
+  propNumber: React.PropTypes.number,
+  propString: React.PropTypes.string,
+  propObject: React.PropTypes.object
+}
+
+Content.defaultProps = {
+  propArray: [1,2,3,4],
+  propBool: true,
+  propFunc: function(e){return e},
+  propNumber: 1,
+  propString: "String value ...",
+  propObject: {
+    objectName1: "objectValue1",
+    objectName2: "objectValue2",
+    objectName3: "objectValue3"
   }
 }
 
@@ -96,7 +103,10 @@ class Clock extends Component{
   constructor(props){
     super(props);
 
-    this.state = {date: new Date()};
+    this.state = {
+      date: new Date(),
+      message: "Hello"
+    };
   }
 
   componentDidMount(){
@@ -107,7 +117,12 @@ class Clock extends Component{
   }
 
   tick(){
-    this.setState({date: new Date()});
+    this.setState(
+      {
+        date: new Date(),
+        message: this.state.message + "x"
+      }
+      );
   }
 
   componentWillUnmount(){
@@ -119,6 +134,7 @@ class Clock extends Component{
       <div>
         <h1>Hello World!</h1>
         <h2>The current time is: {this.state.date.toLocaleTimeString()}</h2>
+        <h2>{this.state.message}</h2>
       </div>
     );
   }
